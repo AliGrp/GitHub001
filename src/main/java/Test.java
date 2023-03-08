9b36ac0464c01ab65d4f95405233f7bd9747ec2d
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-03-08 18:19:24 +0300

    v2

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test 01" );
         System.out.println("Test 02");
+        System.out.println("Test 03");
     }
 }
